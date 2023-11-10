be83ffbee043bfd7f5dde0c62148299cf98116a2
Zein Shhadi <zeinshhadi@hotmail.com>
2023-11-10 23:43:21 +0200

    added comments and fixed creating element

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
+//initializing mostly used elements in the global
 let addTaskButton = document.getElementById("add-task-btn");
-
+//task section is the section where the tasks are scored
 let taskSection = document.getElementById("tasks-section");
-
+//event where a task is added
 addTaskButton.addEventListener("click", function () {
+   //getting value from user input
    let taskTitleInput = document.getElementById("taskTitle");
    let taskDescInput = document.getElementById("taskDesc");
    let taskTitle = taskTitleInput.value;
    let taskDesc = taskDescInput.value;
 
    if (taskTitle && taskDesc) {
-      taskSection.innerHTML += `
-      <div class="task-box" id="taskList" ">
-                     <div class="taskcontaier">
+      //creating and element called div
+      let i = document.createElement("div");
+      //assign class to of task-box
+      i.classList = "task-box";
+      //adding innerHTML div
+      i.innerHTML += `
+  
+                 
                          <div class="task-header">
                              <div class="task-name" id="task-title">${taskTitle}</div>
                              <div class="task-actions">
@@ -24,35 +31,41 @@ addTaskButton.addEventListener("click", function () {
                              <div class="task-description">${taskDesc}</div>
                              <div class="task-mark" id="btn">Mark as Done</div>
                          </div>
-                     </div>
-                     </div>
+              
+                   
                  `;
-
+      //appending the newly created div the section of tasks
+      taskSection.appendChild(i);
+      //emptying input
       taskTitleInput.value = "";
       taskDescInput.value = "";
    } else {
       alert("Empty Values");
    }
 
+   //selecting all elements from the div task-mark and adding
    let markAsDoneButtons = document.querySelectorAll(".task-mark");
-
+   //iterating over each element and assigning eventlistener to eachone
    markAsDoneButtons.forEach(function (currentButton) {
       currentButton.addEventListener("click", doneTask);
    });
 
    function doneTask(event) {
+      //targeting the closet element called (task-mark) from where the event is pressed
       let taskContainer = event.target.closest(".task-mark");
       taskContainer.style.backgroundColor = "green";
    }
-
-   let editTitle = document.querySelectorAll(".edit-name");
-
+   //editing title section
+   let editTitle = document.querySelectorAll(".edit-name"); //adding all elements with edit-name class in a variable
+   //looping over every each element assigning an event listener to it
    editTitle.forEach(function (titleEdit) {
       titleEdit.addEventListener("click", edit);
    });
 
    function edit(event) {
+      //calling this function will set the edit section display from none to inline
       displayPopUp("inline");
+      //targeting the closet task-box div to have access over all other elements of the task (title and description)
       let taskContainer = event.target.closest(".task-box");
       let editTaskButton = document.getElementById("edit-task-btn");
 
@@ -60,21 +73,22 @@ addTaskButton.addEventListener("click", function () {
          let inputs = getInput();
 
          if (inputs.title && inputs.description) {
+            //saving the element holding the title in a variable called taskELementTitle
             let taskElementTitle = taskContainer.querySelector(".task-name");
-
+            //changing or setting the title to new one
             taskElementTitle.innerHTML = inputs.title;
-
+            //saving the element holding the description in a variable called taskELementDesc
             let taskElementDesc = taskContainer.querySelector(".task-description");
-
+            //setting the description to new one
             taskElementDesc.innerHTML = inputs.description;
-
+            //calling this function will set the edit section display from inline back to none
             displayPopUp("none");
          } else {
             alert("Empty Values");
          }
       });
    }
-
+   //selecting all elements from the div delete-task
    let deleteButtons = document.querySelectorAll(".delete-task");
 
    deleteButtons.forEach(function (deleteButton) {
@@ -82,16 +96,18 @@ addTaskButton.addEventListener("click", function () {
    });
 
    function deleteTask(event) {
+      //targeting the  closet task-box the parent parent div of the event button clicked to remove it all from the section
       let taskContainer = event.target.closest(".task-box");
+      //removing the div from the section that is in the taskContainer variable
       taskContainer.remove();
    }
 });
-
+//this function displays the pop up by changing a status parameter if inline will display if none will hide
 function displayPopUp(status) {
    displayWindow = document.querySelector(".edit-form");
    displayWindow.style.display = status;
 }
-
+//get the ipunt from the pop up and adding it to an object to be recieved when called
 function getInput() {
    let newTitle = document.getElementById("editTaskTitle").value;
    let newDesc = document.getElementById("editTaskDesc").value;
